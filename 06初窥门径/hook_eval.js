292db5c5fb1d813061815ae6a0f3d077bbaf32ba
chenrn_window <984598654@qq.com>
2020-01-31 14:22:50 +0800

    无限debug解决方法

---
+++ 06初窥门径/hook_eval.js
@@ -0,0 +1,14 @@
+window.__cr_eval = window.eval
+
+var myeval = function (src) {
+    console.log("========= eval begin: length=" + src.length + ", caller=" + (myeval.caller && myeval.caller.name) + " =======")
+    console.log(">>>>>>>>> eval injected: " + document.location + " <<<<<<<<<")
+    console.log(src)
+    console.log("====== eval end ======")
+    return window.__cr_eval(src)
+}
+
+// 重写eval的toString放置检测出修改了eval
+myeval.toString = function () { return "function eval() { [native code] }" }
+
+Object.defineProperty(window, 'eval', { value: myeval })
